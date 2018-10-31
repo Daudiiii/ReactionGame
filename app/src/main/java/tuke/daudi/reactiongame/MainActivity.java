9e88046a3bcb62b234fc5ef4f1bbc9ea5ced48d5
Daudiiii <trelladavid321@gmail.com>
2018-10-31 13:17:11 +0100

    hide soft items on outside touch

--- app/src/main/java/tuke/daudi/reactiongame/MainActivity.java
+++ app/src/main/java/tuke/daudi/reactiongame/MainActivity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -38,6 +40,15 @@ public class MainActivity extends AppCompatActivity {
         ImageButton stats = findViewById(R.id.ibtn_highscores);
         final TextView txt = findViewById(R.id.textView);
         dbGetData = new DbGetData(new WeakReference<Context>(getApplicationContext()));
+
+        findViewById(R.id.main_activity).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                return true;
+            }
+        });
 
         stats.setOnClickListener(new View.OnClickListener() {
             @Override
