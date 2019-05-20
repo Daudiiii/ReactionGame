87fcdbf75d03afb3e952ace71ea150aa68668db6
Daudiiii <trelladavid321@gmail.com>
2019-05-20 13:34:34 +0200

    Final Update

--- app/src/main/java/tuke/daudi/reactiongame/HighscoresActivity.java
+++ app/src/main/java/tuke/daudi/reactiongame/HighscoresActivity.java
@@ -29,7 +29,6 @@ public class HighscoresActivity extends AppCompatActivity implements ObviousSetu
     private DatabaseReference firebaseReference;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_db_textviews);
         loadAll();
